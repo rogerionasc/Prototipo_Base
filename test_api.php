dc65e9c7b2e6cdbd3fb53cb39cbaba3387e21954
Rogério Nascimento <rogerionascimentosantos@gmail.com>
2026-08-20 08:53:19 -0300

    correção de bug

--- test_api.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-
-$r = app('App\Http\Controllers\GuiaController')->getDadosDaAgenda(39);
-file_put_contents('response.json', $r->getContent());
-echo "Done\n";
--- test_tuss.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-
-$t = Illuminate\Support\Facades\DB::table('tuss')->where('codigo', '15010023')->first();
-echo $t->descricao;
